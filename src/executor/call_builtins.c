8388a66b481caaf0dc4cd025965662419fbdbdd1
mraasvel <mbraasveldt@gmail.com>
2021-03-16 21:33:25 +0100

    added builtin function calls (not quite as I want it though)

--- src/executor/call_builtins.c
+++ src/executor/call_builtins.c
@@ -6,7 +6,7 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/16 08:42:06 by mraasvel      #+#    #+#                 */
-/*   Updated: 2021/03/16 14:52:39 by mraasvel      ########   odam.nl         */
+/*   Updated: 2021/03/16 21:03:24 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,14 @@ int	call_builtins(t_data *data, t_node *node)
 
 	if (ft_strstr(node->args[0], "env") != NULL)
 		exit(ft_env(data));
+	if (ft_strstr(node->args[0], "echo") != NULL)
+		exit(ft_echo(node->args));
+	if (ft_strstr(node->args[0], "pwd") != NULL)
+		exit(ft_pwd(node->args));
 	else if (execve(node->args[0], node->args, data->envp) == -1)
+	{
+		perror("-bash");
 		exit_program(syscall_error, "Execve Error in child process");
+	}
 	return (0);
 }
