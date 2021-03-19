5eb070cb5adba4e5854dcf9682bf587ea7552f53
mraasvel <mbraasveldt@gmail.com>
2021-03-19 08:41:11 +0100

    Finally fixed the git stash

--- src/executor/call_builtins.c
+++ src/executor/call_builtins.c
@@ -6,7 +6,7 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/16 08:42:06 by mraasvel      #+#    #+#                 */
-/*   Updated: 2021/03/19 08:29:11 by mraasvel      ########   odam.nl         */
+/*   Updated: 2021/03/19 08:40:22 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,17 +84,7 @@ int	exec_builtin(t_node *node, t_data *data)
 
 int	call_builtins(t_data *data, t_node *node)
 {
-	static	int	(*builtins[])(t_data*) = {
-		ft_env
-	};
-
-	if (ft_strstr(node->args[0], "env") != NULL)
-		exit(ft_env(data));
-	if (ft_strstr(node->args[0], "echo") != NULL)
-		exit(ft_echo(node->args));
-	if (ft_strstr(node->args[0], "pwd") != NULL)
-		exit(ft_pwd(node->args));
-	else if (execve(node->args[0], node->args, data->envp) == -1)
+	if (execve(node->args[0], node->args, data->envp) == -1)
 	{
 		perror("-bash");
 		exit_program(syscall_error, "Execve Error in child process");
