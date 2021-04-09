8ee04c9f53e28f86444bc26257c1f81e4d80a867
mraasvel <mbraasveldt@gmail.com>
2021-04-09 16:40:36 +0200

    reset terminal on exit

--- src/builtins/exit/ft_exit.c
+++ src/builtins/exit/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/18 13:19:23 by mraasvel      #+#    #+#                 */
-/*   Updated: 2021/04/06 17:28:26 by mraasvel      ########   odam.nl         */
+/*   Updated: 2021/04/09 16:39:29 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <errno.h>
 #include "libft.h"
 #include "structs.h"
+#include "proto.h"
 
 /*
 ** Returns true if all digit
@@ -34,7 +35,7 @@ static t_bool	exit_isnumeric_arg(char *arg)
 	return (ft_true);
 }
 
-static int	exit_perror(char *first, int type)
+static int	exit_exit_perror(char *first, int type)
 {
 	if (type == 1)
 	{
@@ -59,22 +60,23 @@ static int	exit_normal(char *arg)
 	errno = 0;
 	exit_status = ft_strtoll(arg);
 	if (errno == ERANGE)
-		exit(exit_perror(arg, 2));
+		exit(exit_exit_perror(arg, 2));
 	ft_putstr_fd("exit\n", STDOUT_FILENO);
 	exit((unsigned char)exit_status);
 }
 
 int	ft_exit(t_data *data, char **args)
 {
+	reset_term();
 	if (args[1] == NULL)
 	{
 		ft_putstr_fd("exit\n", STDOUT_FILENO);
 		exit((unsigned char)data->exit_status);
 	}
 	else if (!exit_isnumeric_arg(args[1]))
-		exit(exit_perror(args[1], 2));
+		exit(exit_exit_perror(args[1], 2));
 	else if (args[2] != NULL)
-		data->exit_status = exit_perror(NULL, 1);
+		data->exit_status = exit_exit_perror(NULL, 1);
 	else
 		exit_normal(args[1]);
 	return (data->exit_status);
